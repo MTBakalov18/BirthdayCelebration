d3d9e2d6bcc6d3c7b28367b1f90c6580e27e0086
MTBakalov18 <56921192+MTBakalov18@users.noreply.github.com>
2022-10-17 13:50:51 +0300

    Add files via upload

---
+++ Citizen.java
@@ -0,0 +1,37 @@
+public class Citizen implements Identifiable {
+        private String name;
+        private int age;
+        private String id;
+        private String birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
+    }
+}
---
+++ Pet.java
@@ -0,0 +1,17 @@
+public class Pet implements Birthable{
+    String name;
+    String birthDate;
+
+    public Pet(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+}
